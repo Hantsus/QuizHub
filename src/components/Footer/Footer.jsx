396e1bc53cc858515e4bcdd3ee1dcb32a0903f77
Sushant <Sushant9801@gmail.com>
2023-11-18 20:29:13 +0530

    First commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+      <div
+          style={{
+              textAlign: "center",
+              marginBottom: 10,
+          }}
+      >
+          Made with{" "}
+          <a
+              href="#"
+              style={{ cursor: "pointer" ,color:"blueviolet",fontWeight:"500"}}
+          >
+              Sushant
+          </a>
+      </div>
+  );
+}
+
+export default Footer;
+
